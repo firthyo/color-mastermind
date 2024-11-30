dfef1b740c064a014ed579c6fcbbdee8975d081c
firth <fm@goupscale.com>
2024-11-29 16:24:09 -0800

    added deployment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import ColorMaster from "./components/ColorMaster";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <ColorMaster />
+    </>
+  );
+}
+
+export default App;
